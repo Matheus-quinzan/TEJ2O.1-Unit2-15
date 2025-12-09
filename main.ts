a722f55d885ed6037eab33ccd38107976f60fa14
Matheus-quinzan <matheus.quinzan@stu.ocsb.ca>
2025-12-08 19:12:28 -0500

    shows pixels move around the edges on the microbit

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
+ * Created by: Matheus Quinzan
  * Created on: Sep 2020
- * This program ...
+ * This program shows when button A is pressed, the pixels moves around the edge of the Micro:bit
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+
+// setup
+basic.showIcon(IconNames.Duck)
+
+// when "A" is pressed, the pixels move down in a diagnol
+input.onButtonPressed(Button.A, function () {
+    sprite = game.createSprite(0, 0)
+    let sides = 0
+    while (sides < 4) {
+        let steps = 0
+        while (steps < 4) {
+            sprite.move(1)
+            basic.pause(200)
+            steps += 1
+        }
+        sprite.turn(Direction.Right, 90)
+        sides += 1
+    }
+    sprite.delete()
+})
